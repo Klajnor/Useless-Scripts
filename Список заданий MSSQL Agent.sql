8aa712073cde8ce13fa7b2f3d213666e8781ff82
Klajnor <Klajnor@gmail.com>
2020-10-14 17:09:41 +0300

    Update Список заданий MSSQL Agent.sql

--- Список заданий MSSQL Agent.sql
+++ Список заданий MSSQL Agent.sql
@@ -13,6 +13,6 @@ FROM
   msdb.dbo.sysjobs job
     INNER JOIN msdb.dbo.sysjobsteps steps ON job.job_id = steps.job_id
 WHERE
-  job.enabled = 0 -- remove this if you wish to return all jobs
+  job.enabled = 1 -- remove this if you wish to return all jobs
 order by 
   job.name, steps.step_id
