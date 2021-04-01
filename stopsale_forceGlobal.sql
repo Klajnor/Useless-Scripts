34cf19c6cf4309de5390b62272563e2f20219edc
Klajnor <Klajnor@gmail.com>
2021-04-01 18:25:50 +0300

    Create stopsale_forceGlobal.sql

---
+++ stopsale_forceGlobal.sql
@@ -0,0 +1,18 @@
+declare @remote varchar(3) = 'JOY'
+
+update repl_remote set stopsale_forceGlobal = 1 where remote = @remote
+
+while 1 = 1
+begin
+  update top(100) rs set rs.global = 1 from repl_stopsale rs where rs.remote = @remote and rs.global <> 1
+  if @@ROWCOUNT = 0 break
+end
+
+while 1 = 1
+begin
+  update top(100) s set s.global = 1 from stopsale s
+    inner join replcoded rc on rc.remote = @remote and rc.tabals = 'SS' and rc.local_inc = s.inc
+  where
+    rc.remote = @remote and s.global <> 1
+  if @@ROWCOUNT = 0 break
+end
