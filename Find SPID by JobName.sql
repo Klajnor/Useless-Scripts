35aa94d4493d9b6acd82400908a57ab321886b12
Klajnor <Klajnor@gmail.com>
2022-06-10 10:53:42 +0300

    Можно не точно название

--- Find SPID by JobName.sql
+++ Find SPID by JobName.sql
@@ -1,10 +1,30 @@
 /*
-select * from msdb.dbo.sysjobs where name like '%integ%'
+select * from msdb.dbo.sysjobs where name like '%DEA%'
 */
 
-declare @JobName nvarchar(max) = 'Integration. SAMO-incoming load class'
+declare @JobName nvarchar(max) = '%DEA%'
 
-declare @jobID uniqueidentifier
-select @jobID = job_id from msdb.dbo.sysjobs where name = @JobName
+declare @Jobs table (
+    name nvarchar(max)
+  , job_id uniqueidentifier
+  , spid int
+)
 
-select * from sys.sysprocesses where program_name like '%' + master.sys.fn_varbintohexstr(@jobID) + '%'
+insert into @Jobs (
+    name
+  , job_id
+  , spid
+)
+select
+    j.name
+  , j.job_id
+  , s.spid
+from msdb.dbo.sysjobs j
+  outer apply (select top 1 * from sys.sysprocesses where program_name like '%' + master.sys.fn_varbintohexstr(j.job_id) + '%') s
+where 1 = 1
+  and j.name like @JobName
+
+select * from @Jobs j
+order by j.name
+
+--sp_whoisactive 308
