f612665c34c61dd139367e1815940b8f27c148c7
Klajnor <Klajnor@gmail.com>
2022-06-16 17:21:41 +0300

    Update История отправки заявки и получения статуса подтверждения.sql
    Добавил up_repl_getclaimstatus

--- История отправки заявки и получения статуса подтверждения.sql
+++ История отправки заявки и получения статуса подтверждения.sql
@@ -1,8 +1,12 @@
+declare @claim int = 8753555
+
+exec up_repl_getclaimstatus @claim
+
 select 
   *
 from [dbo].[repl_SendClaim_log] l
   left join [dbo].[repl_SendClaim_log_proc] p on p.inc = l.repl_log_procId
   left join [dbo].[repl_SendClaim_log_desc] d on d.inc = l.repl_log_descId
 where
-  l.claim = 5495112
+  l.claim = @claim
 order by l.inc desc
