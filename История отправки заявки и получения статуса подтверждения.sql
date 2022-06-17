149da23143c4d7d4575b85a498e87cb84dd3b0fc
Klajnor <Klajnor@gmail.com>
2022-06-17 12:54:26 +0300

    Update История отправки заявки и получения статуса подтверждения.sql

--- История отправки заявки и получения статуса подтверждения.sql
+++ История отправки заявки и получения статуса подтверждения.sql
@@ -10,3 +10,15 @@ from [dbo].[repl_SendClaim_log] l
 where
   l.claim = @claim
 order by l.inc desc
+
+
+-- Общий лог
+select top 100
+  *
+from [dbo].[repl_SendClaim_log] l
+  left join [dbo].[repl_SendClaim_log_proc] p on p.inc = l.repl_log_procId
+  left join [dbo].[repl_SendClaim_log_desc] d on d.inc = l.repl_log_descId
+where 1 = 1
+  and l.remote = 'TUR'
+  and l.claim = 0
+order by l.inc desc
