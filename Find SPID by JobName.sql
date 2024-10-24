f23b47353cdb6d248de49561c8b5ee881366feb9
Klajnor <Klajnor@gmail.com>
2024-10-24 21:28:00 +0300

    Update Find SPID by JobName.sql

--- Find SPID by JobName.sql
+++ Find SPID by JobName.sql
@@ -2,23 +2,29 @@
 select * from msdb.dbo.sysjobs where name like '%DEA%'
 */
 
-declare @JobName nvarchar(max) = '%LGO%'
+declare @JobName nvarchar(max) = '%AL2%'
 
 declare @Jobs table (
     name nvarchar(max)
   , job_id uniqueidentifier
   , spid int
+  , login_time datetime
+  , last_batch datetime
 )
 
 insert into @Jobs (
     name
   , job_id
   , spid
+  , login_time
+  , last_batch
 )
 select
     j.name
   , j.job_id
   , s.spid
+  , s.login_time
+  , s.last_batch
 from msdb.dbo.sysjobs j
   outer apply (select top 1 * from sys.sysprocesses where program_name like '%' + master.sys.fn_varbintohexstr(j.job_id) + '%') s
 where 1 = 1
