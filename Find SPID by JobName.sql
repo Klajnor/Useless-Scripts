d4aed0831d13c58fac1465e679f47a6d94ddda12
Klajnor <Klajnor@gmail.com>
2024-10-25 09:39:46 +0300

    Update Find SPID by JobName.sql

--- Find SPID by JobName.sql
+++ Find SPID by JobName.sql
@@ -9,7 +9,9 @@ declare @Jobs table (
   , job_id uniqueidentifier
   , spid int
   , login_time datetime
+  , diff_ms_login_time_to_now int
   , last_batch datetime
+  , diff_ms_last_batch_to_now int
 )
 
 insert into @Jobs (
@@ -17,21 +19,26 @@ insert into @Jobs (
   , job_id
   , spid
   , login_time
+  , diff_ms_login_time_to_now
   , last_batch
+  , diff_ms_last_batch_to_now
 )
 select
     j.name
   , j.job_id
   , s.spid
   , s.login_time
+  , datediff(second, s.login_time, getdate()) diff_ms_login_time_to_now
   , s.last_batch
+  , datediff(second, s.last_batch, getdate()) diff_ms_last_batch_to_now
 from msdb.dbo.sysjobs j
   outer apply (select top 1 * from sys.sysprocesses where program_name like '%' + master.sys.fn_varbintohexstr(j.job_id) + '%') s
 where 1 = 1
   and j.name like @JobName
 
 select
-    'sp_whoisactive @filter = ' + convert(varchar(16), spid) + ', @get_plans = 1'
+    'sp_whoisactive @filter = ' + convert(varchar(16), spid) + ', @get_plans = 1' sp_whoisactive_sql
+  , 'kill ' + convert(varchar(16), spid) kill_sql
   , *
 from @Jobs j
 order by j.name
