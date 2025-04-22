4935867726dd3ff5d994fd4f39b9091caa864033
Klajnor <Klajnor@gmail.com>
2025-04-22 17:39:29 +0300

    Update Поиск по хранимкам.sql

--- Поиск по хранимкам.sql
+++ Поиск по хранимкам.sql
@@ -1,3 +1,4 @@
-SELECT [type], [name], OBJECT_DEFINITION([object_id])
+SELECT [type], quotename(SCHEMA_NAME(schema_id)) + '.' + QUOTENAME([name]), OBJECT_DEFINITION([object_id])
 FROM sys.objects
-WHERE [type] IN ('P','TR','FN') AND OBJECT_DEFINITION([object_id]) LIKE N'%Your text for search%';
+WHERE [type] IN ('P','TR','FN') AND OBJECT_DEFINITION([object_id]) LIKE N'%hotelservice%' -- сюда пиши часть кода
+order by quotename(SCHEMA_NAME(schema_id)) + '.' + QUOTENAME([name]) 
