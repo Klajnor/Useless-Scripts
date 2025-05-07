b817792e8f4794df9371205bc313da2f1e1c5e7f
Klajnor <Klajnor@gmail.com>
2025-05-07 11:17:21 +0300

    Update ch.history.sql

--- ch.history.sql
+++ ch.history.sql
@@ -3,6 +3,8 @@ select
 from ch.history h
   cross apply ch.uf_showhistorychanges(h.inc) ch
 where
-  h.stable = 'partner' -- record table
-  and h.sinc = 262 -- record id
+  h.stable = 'claim' -- record table
+  and h.sinc = 90322989 -- record id
   --and ch.Field like 'sendmessage%' 
+order by 
+    h.inc desc
